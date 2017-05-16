2d1bcfb05ffb0648db4acd3d265b903a9271d8a7
Ivan Tugay <listepo@gmail.com>
2017-05-16 17:09:17 +0300

    Added map markers

--- src/pages/restaurants/restaurants.ts
+++ src/pages/restaurants/restaurants.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, Platform } from 'ionic-angular';
 import {
  GoogleMaps,
  GoogleMap,
@@ -17,53 +17,54 @@ import {
 })
 export class RestaurantsPage {
   public displayType = 'map';
-  public restaurants = [
-    { address: 'пр-т Перемоги, 9-б', phone: '063 356 05 93' },
-    { address: 'бульв. Лесі Українки, 34', phone: '073 311 63 30' },
-    { address: 'вул. Мішуги, 3-в', phone: '063 356 01 34' },
-    { address: 'бульвар Вацлава Гавела, 6', phone: '073 311 63 04' },
-    { address: 'пр-т Маяковського, 43/2', phone: '073 311 62 99' },
-  ];
-  constructor(private googleMaps: GoogleMaps) {}
-  public ngAfterViewInit() {
-    this.loadMap();
+  public get isMapType() {
+    return this.displayType === 'map';
   }
+  public get isListType() {
+    return this.displayType === 'list';
+  }
+  public restaurants = [
+    { address: 'пр-т Перемоги, 9-б', phone: '063 356 05 93', location: {lat: 50.447193, lng: 30.483035} },
+    { address: 'бульв. Лесі Українки, 34', phone: '073 311 63 30', location: {lat: 50.4214396, lng: 30.5445165} },
+    { address: 'вул. Мішуги, 3-в', phone: '063 356 01 34', location: {lat: 50.3966242, lng: 30.6385013} },
+  ];
+  private map: GoogleMap;
+  constructor(public googleMaps: GoogleMaps, public platform: Platform) {}
+
+  public ngAfterViewInit() {
+    this.platform.ready().then(() => {
+      this.loadMap();
+    });
+  }
+
   public loadMap() {
     const element: HTMLElement = document.getElementById('restaurants-map');
+    this.map = this.googleMaps.create(element);
 
-    const map: GoogleMap = this.googleMaps.create(element);
+    this.map.on(GoogleMapsEvent.MAP_READY).subscribe(() => {
+      this.addMarkers();
+    });
 
-    // listen to MAP_READY event
-    // You must wait for this event to fire before adding something to the map or modifying it in anyway
-    map.one(GoogleMapsEvent.MAP_READY).then(
-      () => {
-        console.log('Map is ready!');
-        // Now you can add elements to the map like the marker
-      },
-    );
+    const defaultPosition: LatLng = new LatLng(50.45, 30.523333);
 
-    // create LatLng object
-    const ionic: LatLng = new LatLng(43.0741904, -89.3809802);
-
-    // create CameraPosition
     const position: CameraPosition = {
-      target: ionic,
-      zoom: 18,
+      target: defaultPosition,
+      zoom: 10,
       tilt: 30,
     };
+    this.map.moveCamera(position);
 
-    // move the map's camera to position
-    map.moveCamera(position);
+  }
 
-    // create new marker
-    const markerOptions: MarkerOptions = {
-      position: ionic,
-      title: 'Ionic',
-    };
-
-    map.addMarker(markerOptions)
-      .then((addedMarker: Marker) => {
-          addedMarker.showInfoWindow();
-    });
+  public addMarkers() {
+    for (const restaurant of this.restaurants) {
+      const {lat, lng} = restaurant.location;
+      const position = new LatLng(lat, lng);
+      const markerOptions: MarkerOptions = {
+          position,
+          title: restaurant.phone,
+      };
+      this.map.addMarker(markerOptions);
+    }
   }
 }
